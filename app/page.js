774b99e5a3cb41cba26a09cce89af15c8189ba5d
coderFrankenstain <453628567@qq.com>
2023-08-31 12:53:36 +0800

    灯管5

--- app/page.js
+++ app/page.js
@@ -4,7 +4,11 @@ import ColorSetter from "./components/ColorSetter";
 function App() {
   return (
     <div className="App">
-      <h1>灯光合成</h1>
+      <h1>
+        任务：
+        <br />
+        通过移动三个滑动条改变三原色的比例，合成灯管的新颜色。
+      </h1>
       <ColorSetter />
     </div>
   );
